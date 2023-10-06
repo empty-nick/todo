513eb7d442dbc0bc114cef51243745cc51cbce88
aniretakey <EMKornishina@gmail.com>
2023-10-06 18:54:58 +0300

    feat: update style

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,20 @@
-import { Modal } from "../../components/Modal/Modal.tsx";
-import { useState } from "react";
+import { Modal } from '../../components/Modal/Modal.tsx';
+import { useState } from 'react';
+import { Task } from '../../components/Task/Task';
 
 export function Home() {
-	const [visible, setVisible] = useState(false)
-	const toggleModal = () => {
-		setVisible(true)
-		console.log(visible)
-	}
-	return (
-		<div>
-			<Modal isVisible={visible} setVisible={setVisible} />
-			<button onClick={toggleModal}>Toggle</button>
-		</div>
-	)
+  const [visible, setVisible] = useState(false);
+  const toggleModal = () => {
+    setVisible(true);
+    console.log(visible);
+  };
+  return (
+    <>
+      <div>
+        <Modal isVisible={visible} setVisible={setVisible} />
+        <button onClick={toggleModal}>Toggle</button>
+        <Task />
+      </div>
+    </>
+  );
 }
