e9d7038936bebd56ec5ade3bd016a17c4917f401
empty-nick <leard246@gmail.com>
2023-10-06 18:32:52 +0300

    SMTH

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,12 +3,19 @@ import { useRoutes } from "react-router-dom";
 import { routes } from "../../pages/routes.tsx";
 
 import styles from './styles.module.css'
+import { Header } from "../Header/Header.tsx";
+import { Footer } from "../Footer/Footer.tsx";
 
 function App() {
   const route = useRoutes(routes)
+  console.log(styles.content)
   return (
     <div className={styles.global}>
-      {route}
+      <Header style={styles.header} />
+      <div className={styles.content}>
+        {route}
+      </div>
+      <Footer style={styles.footer} />
     </div>
   )
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,14 +2,17 @@ import styles from './styles.module.css'
 import { useState } from "react";
 import { Modal } from "../Modal/Modal.tsx";
 
-export function Header() {
+interface IHeader{
+	style: string;
+}
+export function Header({style}: IHeader) {
 	const [visible, setVisible] = useState(false)
 	const toggleModal = () => {
 		setVisible(true)
 		console.log(visible)
 	}
 	return (
-		<div className={styles.header}>
+		<div className={`${style} ${styles.header}`}>
 			Header
 			<button onClick={toggleModal}>Open</button>
 			<Modal isVisible={visible} setVisible={setVisible} />
