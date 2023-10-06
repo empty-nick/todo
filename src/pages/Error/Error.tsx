69f1c9559ebf68404cc7e4973650ad2b62daceef
Ivan Fedotovsky <fedotovsky96@gmail.com>
2023-10-06 19:23:59 +0400

    add navigation

--- src/pages/Error/Error.tsx
+++ src/pages/Error/Error.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import errorImage from "../../assets/Errors/404.png";
 import styles from "./Error.module.css";
 
@@ -6,6 +7,9 @@ export function Error() {
     <div className={styles.error}>
       <img className={styles.errorImage} src={errorImage} alt="pageNotFound" />
       <h4 className={styles.errorText}>Sorry, page not found...</h4>
+      <button className={styles.errorBtn}>
+        <Link to="/">Back to Home</Link>
+      </button>
     </div>
   );
 }
