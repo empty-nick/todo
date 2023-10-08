6b841e6c3e14dca74ded829d3fabc9b5fa2ec81b
Ivan Fedotovsky <fedotovsky96@gmail.com>
2023-10-08 17:42:54 +0400

    add: Weather

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,14 +2,17 @@ import styles from "./styles.module.css";
 import { NavLink } from "react-router-dom";
 
 interface IHeader {
-	style: string;
+  style: string;
 }
 
 export function Header({ style }: IHeader) {
-	return (<div className={`${style} ${styles.header}`}>
-		<nav className={styles.header_navigation}>
-			<NavLink to="/">Home</NavLink>
-			<NavLink to="/error">Error</NavLink>
-		</nav>
-	</div>);
+  return (
+    <div className={`${style} ${styles.header}`}>
+      <nav className={styles.header_navigation}>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/weather">Weather</NavLink>
+        <NavLink to="/error">Error</NavLink>
+      </nav>
+    </div>
+  );
 }
